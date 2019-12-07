c7617056adf914c1957550d6d76d15e3bed7ba9b
paulo.jave.c@gmail.com <LPJave31548378>
2019-12-07 06:54:04 -0500

    Carga 20191207

--- PLQL_modulo1.sql
+++ PLQL_modulo1.sql
@@ -1,3 +1,7 @@
+CONCEPTOS GENERALES
+--PLSQL se ejecuta en su propio motor
+--SQL se ejecuta en su propio motor
+
 1) DESBLOQUEAR USUARIO HR:	
 
 	--Desbloquea usuario HR 
@@ -696,11 +700,398 @@
     END;
     
     
-21)
-
+21)Selects en PLSQL
+   --El INTO siempre espera retornar algo, si el select devuelve nulo o nada, hara un error
+   --Eso se soluciona con cursores y/o excepciones
+   --salario y nombre esperan una unica respuesta, si el select devuelve mas de un registro caera en error
+    
+    DECLARE
+        salario   NUMBER;
+        nombre employees.first_name%TYPE;
+    BEGIN
+        SELECT  --SOLO PUEDE DEVOLVER UNA FILA
+            salary,first_name INTO salario,nombre
+        FROM
+            employees
+        WHERE
+            employee_id = 200;
+        dbms_output.put_line(salario);
+        dbms_output.put_line(nombre);
+    END;
     
     
+22) EL %ROWTYPE
+    --Sirve para traer un objeto y no declarar todas las columnas
+    -- En el caso que asignes un valor a una columna esata no se refleja en la base de datos, solo aplica para el dato en memoria
+
+    DECLARE
+        salario   NUMBER;
+        nombre EMPLOYEES.FIRST_NAME%TYPE;
+        empleado EMPLOYEES%ROWTYPE; -- Trea un objeto de la misma estrutura de empleado
+    BEGIN
+        SELECT  --SOLO PUEDE DEVOLVER UNA FILA
+            * INTO empleado
+        FROM
+            employees
+        WHERE
+            employee_id = 100;
+        dbms_output.put_line(empleado.SALARY*100);
+        dbms_output.put_line(empleado.FIRST_NAME);
+    END;
+    
+23) PRÁCTICAS CON SELECT INTO
+    
+    --Crear un bloque PL/SQL que devuelva al salario máximo del departamento 100 y lo deje  en una variable denominada salario_maximo y la visualice
+    declare
+        salario_maximo employees.salary%TYPE;
+    begin
+        select max(emp.salary) INTO salario_maximo from employees emp;
+        dbms_output.put_line('El salario maximo es: ' || salario_maximo);
+    end;
+    
+    --Visualizar el tipo de trabajo del empleado número 100
+    declare
+        tipo_trabajo jobs.job_title%TYPE;
+    begin
+        select tra.job_title INTO tipo_trabajo from employees emp inner join jobs tra on emp.job_id = tra.job_id 
+        where emp.employee_id = 100;
+        dbms_output.put_line('El tipo de trabajo es: ' || tipo_trabajo);
+    end;
+    
+    --Crear una variable de tipo DEPARTMENT_ID y ponerla algún valor, por ejemplo 10.
+    --Visualizar el nombre de ese departamento y el número de empleados que tiene, poniendo. Crear dos variables para albergar los valores.
+    declare
+        id_departamento departments.department_id%TYPE := 30;
+        nombre_departamento departments.department_name%TYPE;
+        nro_empleados Number;
+    begin
+        select dep.department_name, count(emp.department_id) INTO nombre_departamento, nro_empleados from employees emp inner join departments dep on emp.department_id = dep.department_id 
+        where emp.department_id = id_departamento group by dep.department_name;
+        dbms_output.put_line('Departamento : ' || nombre_departamento);
+        dbms_output.put_line('Cantidad empleados : ' || nro_empleados);
+    end;
+    
+    --Mediante dos consultas recuperar el salario máximo y el salario mínimo de la empresa e indicar su diferencia
+    declare
+        salMax  employees.salary%TYPE;
+        salMin employees.salary%TYPE;
+        salDif employees.salary%TYPE;
+    begin
+        select max(emp.salary) INTO salMax from employees emp;
+        select min(emp.salary) INTO salMin from employees emp;
+        saldif := salMax - salMin;
+        dbms_output.put_line('Maximo : ' || salMax);
+        dbms_output.put_line('Minimo : ' || salMin);
+        dbms_output.put_line('Diferencia : ' || salDif);
+    end;
+
+24) Insert en PLSQL
+    
+    declare
+        col1 test.c1%TYPE;
+    begin
+        col1 := 11;
+        insert into test (c1, c2) values (col1,'JOJO');
+        commit;
+    end;
+    
+24) Update en PLSQL
+    
+    declare
+        col1 test.c1%TYPE;
+    begin
+        col1 := 11;
+        update test set c2 = 'aaaa' where test.c1 = col1;
+        commit;
+    end;
+    
+24) Delete en PLSQL
+    
+    declare
+        col1 test.c1%TYPE;
+    begin
+        col1 := 11;
+        delete from test where test.c1 = col1;
+        commit;
+    end;
 
 
+25) Practica de insert, update y delete en PLSQL
+
+    /*Crear un bloque inserte un nuevo departamento en la tabla DEPARTMENTS.. Para saber el DEPARTMENTI_ID que debemos asignar al nuevo departamento primero debemos averiguar el valor mayor que hay en la tabla DEPARTMENTS y sumarle uno para la nueva clave.
+    
+    o Location_id debe ser 1000
+    o Manager_id debe ser 100
+    o Department_name debe ser “INFORMATICA”
+    o NOTA: en PL/SQL debemos usar COMMIT y ROLLBACK de la misma forma que lo hacemos en SQL. Por tanto, para validar definitivamente un cambio debemos usar COMMIT. Podemos
+    */
+    DECLARE
+        MAX_ID NUMBER;
+    BEGIN
+        SELECT MAX(DEPARTMENT_ID) INTO MAX_ID FROM DEPARTMENTS;
+        MAX_ID:=MAX_ID+1;
+        INSERT INTO departments (department_id,department_name,manager_id,location_id) VALUES (MAX_ID,'INFORMATICA',100,1000);
+        COMMIT;
+    END;   
+    
+    /*Creamos un bloque PL/SQL que modifique la LOCATION_ID del nuevo departamento a 1700. En este caso usemos el COMMIT dentro del bloque PL/SQL.*/
+    BEGIN
+        UPDATE DEPARTMENTS SET LOCATION_ID=1700 WHERE DEPARTMENT_NAME='INFORMATICA';
+        COMMIT;
+    END;
+    
+    /*Por último hacer otro bloque PL/SQL que elimine ese departamento nuevo.*/    
+    BEGIN
+        DELETE DEPARTMENTS WHERE DEPARTMENT_NAME='INFORMATICA';
+        COMMIT;
+    END;
 
 
+26) Manejo de excepciones
+    --El control de excepciones es el mecanismo del PL/SQL para manejar errores de tiempo de ejecución. 
+    --El utilizar este mecanismo permite continuar la ejecución del programa, si el error no es muy grave, lo cual quedaría a decisión del programador.
+    -- Los errores son llamados excepciones
+    -- Se peude crear excepciones para controlar partes de la progrmacion
+    --No siempre el error esta donde muestra la consola
+    --Hay dos tipos de excepiones:
+        --A)Excepciones definidas por el usuario: El PL/SQL da la posibilidad de que el usuario defina excepciones en área de especificaciones. Vea el ejemplo: ot_failure EXCEPTION; 
+          --En este caso el nombre de la excepción es ot_failure. El código asociado que maneja la excepción es:
+
+            EXCEPTION
+              WHEN OT_FAILURE THEN
+            ...
+            
+        --B)Excepciones del sistema: Estas son las excepciones predefinidas del sistema. Un ejemplo de ellas es el NO_DATA_FOUND, que se dispara cuando un SELECT-INTO no obtiene registros, veamos:
+            DECLARE
+              v_empid emp.empid%TYPE;
+            BEGIN
+              ...
+              SELECT e.empid INTO v_empid  
+              FROM emp e
+              WHERE e.ename = 'CHARLIE';
+            EXCEPTION
+              WHEN NO_DATA_FOUND THEN
+               RAISE OT_FAILURE;
+              WHEN TOO_MANY_ROWS THEN
+               RAISE OT_FAILURE;
+            END;
+            --En este caso cuando el SELECT no devuelve columnas o devuelve más de una se levanta una excepción que a su vez es atrapada para ser reenviada a una excepción definida por el usuario.
+        
+        --C)Adicional a las excepciones anteriores existe una más llamada OTHERS, que atrapa cualquier excepción que no haya sido establecida explícitamente.
+              WHEN OTHERS THEN
+              out_status_code := 'FAIL';
+              out_msg := g_out_msg || ' ' || SUBSTR(SQLERRM, 1, 60);
+              --SQLERRM es válido cuando se invoca dentro de un manejador de excepción, en otro caso debe pasarse como parámetro el código del error.
+    
+        
+    declare
+        empl employees%ROWTYP; --PLS-00208: el identificador 'ROWTYP' no es un atributo de cursor válido
+    begin
+        select * into empl from employees where employees.employee_id = 1000;
+        dbms_output.put_line('Nombre : ' || empl.first_name);
+    end;
+    
+    declare
+        empl employees%ROWTYPE; 
+    begin
+        select * into empl from employees where employees.employee_id = 1000;
+        dbms_output.put_line('Nombre : ' || empl.first_name); 
+    end; -- ORA-01403: No se ha encontrado ningún dato
+    
+
+
+27) Sintaxis de Excepciones
+
+    DECLARE
+        empl   employees%rowtype;
+    BEGIN
+        SELECT * INTO empl FROM employees WHERE employee_id > 1;    
+        dbms_output.put_line(empl.first_name);
+    EXCEPTION --Se viene los conjuntos de flujos de control
+        WHEN ex1 THEN
+            NULL;
+        WHEN ex2 THEN
+            NULL;
+        WHEN OTHERS THEN
+            NULL;
+    END;
+    
+    
+28) Excepciones predefinidas
+    --Lista de algunos errores oracle
+    --https://docs.oracle.com/cd/B10500_01/appdev.920/a96624/07_errs.htm
+
+
+    SET SERVEROUTPUT ON
+    DECLARE
+        EMPL EMPLOYEES%ROWTYPE;
+    BEGIN
+        SELECT * INTO EMPL
+        FROM EMPLOYEES
+        WHERE EMPLOYEE_ID>1;
+        
+        DBMS_OUTPUT.PUT_LINE(EMPL.FIRST_NAME);
+    EXCEPTION
+    -- NO_DATA_FOUND   ORA-01403
+    -- TOO_MANY_ROWS    --Trae mas de una consulta
+    -- ZERO_DIVIDE      --Cuando quieres dividir entre 0
+    -- DUP_VAL_ON_INDEX --Cunado la clave ya existe unica o primaria
+       WHEN NO_DATA_FOUND THEN 
+           DBMS_OUTPUT.PUT_LINE('ERROR, EMPLEADO INEXISTENTE');
+      WHEN TOO_MANY_ROWS THEN
+            DBMS_OUTPUT.PUT_LINE('ERROR, DEMASIADOS EMPLEADO');
+       WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('ERROR INDEFINIDO');
+    END;
+    
+28) Excepciones no predefinidas
+    --Son aquellas que nos on defeinidas.
+    --Primero debemos de identificar el codigo de error para localizar, lo recomendable es buscar por nro de error    
+    --https://docs.oracle.com/pls/db92/db92.error_search?remark=homepage&prefill=ORA-
+    --Siguen siendo excepciones de oracle, osea no estan predefinidas y no son credas por nosotros los usuarios finales
+    --Todos los error de oracle son negativo, empiesan con el menos (-) a excepcion del 100
+    
+    SET SERVEROUTPUT ON
+    DECLARE
+       MI_EXCEP EXCEPTION; --Creacion de varible
+       PRAGMA EXCEPTION_INIT(MI_EXCEP,-937); --indicamos en codigo de error de oracle
+       V1 NUMBER;
+       V2 NUMBER;
+    BEGIN
+        SELECT EMPLOYEE_ID,SUM(SALARY) INTO V1,V2 FROM EMPLOYEES; --Este error se debe a que sum salary devuelve un resultado
+        DBMS_OUTPUT.PUT_LINE(V1);
+    EXCEPTION
+       WHEN MI_EXCEP THEN 
+           DBMS_OUTPUT.PUT_LINE('FUNCION DE GRUPO INCORRECTA');
+       WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('ERROR INDEFINIDO');
+    END;    
+
+29) SQLCODE y SQLERRM
+    --Solo se maneja errores SQL, mas no PLSQL
+    --SQLCODE= devuelve el error de mensaje de SQL
+    --SQLERRM= devuelve el mensaje de error SQL
+    
+    SET SERVEROUTPUT ON
+    DECLARE
+       EMPL EMPLOYEES%ROWTYPE;
+       CODE NUMBER;
+       MESSAGE VARCHAR2(100);
+    BEGIN
+       SELECT * INTO EMPL FROM EMPLOYEES;
+        DBMS_OUTPUT.PUT_LINE(EMPL.SALARY);
+    EXCEPTION   
+       WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            --INSERT INTO ERRORS VALUES (SQLCODE,SQLERRM); --Esto arroja error por que el INSERT se ejecuta en el motor SQL. En cambio SQLCODE y SQLERRM se ejecutan en el motor PLSQL
+            CODE:=SQLCODE;
+            MESSAGE:=SQLERRM;
+            INSERT INTO ERRORS VALUES (CODE,MESSAGE);
+            COMMIT;
+    END;
+    
+30) Practica excepciones
+
+    --1) Crear una SELECT (no un cursor explícito) que devuelva el nombre de un empleado pasándole el EMPLOYEE_ID en el WHERE,  
+    --Comprobar en primer lugar que funciona pasando un empleado existente 
+    --Pasar un empleado inexistente y comprobar que genera un error
+    --Crear una zona de EXCEPTION controlando el NO_DATA_FOUND para que pinte un mensaje como “Empleado inexistente”
+    declare
+        idEmpleado employees.employee_id%TYPE := 200;
+        nombreEmpleado employees.first_name%TYPE;
+    begin
+        select employees.first_name INTO nombreEmpleado FROM employees where employees.employee_id = idEmpleado;
+        DBMS_OUTPUT.PUT_LINE('El nombre del empleado es: ' || nombreEmpleado);
+    exception
+        WHEN NO_DATA_FOUND THEN 
+        DBMS_OUTPUT.PUT_LINE('ERROR, EMPLEADO INEXISTENTE');
+        WHEN OTHERS THEN 
+        DBMS_OUTPUT.PUT_LINE('ERROR INDEFINIDO');          
+    end;
+    
+    --2) Modificar la SELECT para que devuelva más de un empleado, por ejemplo poniendo EMPLOYEE_ID> 100. Debe generar un error. Controlar la excepción para que genere un mensaje como “Demasiados empleados en la consulta”
+    declare
+        idEmpleado employees.employee_id%TYPE := 200;
+        nombreEmpleado employees.first_name%TYPE;
+    begin
+        select employees.first_name INTO nombreEmpleado FROM employees where employees.employee_id > idEmpleado;
+        DBMS_OUTPUT.PUT_LINE('El nombre del empleado es: ' || nombreEmpleado);
+    exception
+        WHEN TOO_MANY_ROWS THEN 
+        DBMS_OUTPUT.PUT_LINE('ERROR, DEMASIADOS EMPLEADOS EN LA CONSULTA');
+        WHEN OTHERS THEN 
+        DBMS_OUTPUT.PUT_LINE('ERROR INDEFINIDO');          
+    end;
+    
+    --3)Modificar la consulta para que devuelva un error de división por CERO, por ejemplo, vamos a devolver el salario en vez del nombre y lo dividimos por 0. En este caso, en vez de controlar la excepción directamente, creamos una sección WHEN OTHERS y pintamos el error con SQLCODE y SQLERR
+    declare
+        idEmpleado employees.employee_id%TYPE := 200;
+        salarioempelado employees.salary%TYPE;
+    begin
+        select employees.salary INTO salarioempelado FROM employees where employees.employee_id = idEmpleado;
+        DBMS_OUTPUT.PUT_LINE('El salario del empleado es: ' || salarioempelado / 0);
+    exception
+        WHEN OTHERS THEN 
+        DBMS_OUTPUT.PUT_LINE('ERROR ZERO_DIVIDE');  
+        DBMS_OUTPUT.PUT_LINE(SQLCODE);
+        DBMS_OUTPUT.PUT_LINE(SQLERRM);
+    end;
+    
+    --4)El error -00001 es clave primaria duplicada. Aunque ya existe una predefinida (DUP_VAL_ON_INDEX) vamos a crear una excepción no -predefinida que haga lo mismo. o Vamos a usar la tabla REGIONS para hacerlo más fácil o Usamos PRAGMA EXCEPTION_INIT y creamos una excepción denominada “duplicado”. Cuando se genere ese error debemos pintar “Clave duplicada, intente otra”. o Insertamos una fila en la tabla REGIONS que esté duplicada y vemos que se controla el error*/
+    DECLARE
+        duplicado EXCEPTION;
+        PRAGMA EXCEPTION_INIT(duplicado,-00001);
+    BEGIN
+        INSERT INTO REGIONS VALUES (1,'PRUEBA');
+        COMMIT;
+    EXCEPTION
+        when duplicado then
+        dbms_output.put_line('Registro duplicado');
+    End;
+    
+    
+31) Excepciones que no son error
+    --Trataremos excepciones por consultas SQL pero que no son error, sino, seran puntos de control
+
+SET SERVEROUTPUT ON
+DECLARE
+  REG REGIONS%ROWTYPE;
+  REG_CONTROL REGIONS.REGION_ID%TYPE;
+BEGIN
+   REG.REGION_ID:=100;
+   REG.REGION_NAME:='AFRICA';
+   SELECT REGION_ID INTO REG_CONTROL FROM REGIONS
+   WHERE REGION_ID=REG.REGION_ID;
+   DBMS_OUTPUT.PUT_LINE('LA REGION YA EXISTE');
+EXCEPTION   
+   WHEN NO_DATA_FOUND  THEN
+        INSERT INTO REGIONS VALUES (REG.REGION_ID,REG.REGION_NAME);
+        COMMIT;
+END; 
+
+
+32) Excepciones personalizadas
+    --No insertar regiones mayor a 100
+    --
+    DECLARE
+       reg_max EXCEPTION;
+       regn NUMBER;
+       regt varchar2(200);
+    BEGIN
+       regn:=101;
+       regt:='ASIA';
+       IF regn > 100 THEN
+             RAISE reg_max;  -- Raise, clausula se usa para excepciones personalizadas, excepciones de usurio
+        ELSE
+           insert into regions values (regn,regt);
+           commit;
+          END IF;
+    EXCEPTION
+      WHEN reg_max THEN  
+        DBMS_OUTPUT.PUT_LINE('La region no puede ser mayor de 100.');
+      WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error indefinido');
+    END;
+
+    
